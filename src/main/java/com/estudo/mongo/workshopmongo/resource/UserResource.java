e6e66e974663a66ec4c71bac067a068345727d52
edguedes <menuseu@gmail.com>
2018-12-26 23:03:29 -0300

    Criacao da estrutura REST e User DTO para response

--- src/main/java/com/estudo/mongo/workshopmongo/resource/UserResource.java
+++ src/main/java/com/estudo/mongo/workshopmongo/resource/UserResource.java
@@ -1,27 +1,29 @@
 package com.estudo.mongo.workshopmongo.resource;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.estudo.mongo.workshopmongo.dto.UserDTO;
 import com.estudo.mongo.workshopmongo.entity.User;
+import com.estudo.mongo.workshopmongo.service.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userservice;
+	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria Silva", "maria@teste.com");
-		User ze = new User("2", "Ze Silva", "ze@teste.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, ze));
-		return ResponseEntity.ok().body(list);  
-		}
+	public ResponseEntity<List<UserDTO>> findAll(){
+		List<User> list = userservice.findAll();
+		List<UserDTO> listdto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listdto);  
+	}
 }
